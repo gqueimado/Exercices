42fc30892e828cec807f13916bb65e8226c78c52
gqueimado <goncalo.queimado@alten.pt>
2018-02-07 15:49:51 +0000

    New

--- src/main/java/BasePage.java
+++ src/main/java/BasePage.java
@@ -2,7 +2,6 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BasePage {
     public static WebDriver driver;
